b20383bb2f31c5bf706bb0cec4bf5f8b23185542
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2025-06-14 15:51:26 +0200

    dashboard, made categories tabs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com'
+        ]);
         $categories = [
             'Technology',
             'Health',
